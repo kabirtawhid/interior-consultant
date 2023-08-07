81babb28fee55da2980ae277d9d7a8f6bffd5f49
Kabir Tawhid <kabirfromtawhid@gmail.com>
2023-08-07 15:52:56 +0600

    Completed styling logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    fontFamily: {
+      montserrat: ["Montserrat"],
+      "crimson-pro": ["Crimson Pro"],
+      lora: ["Lora"],
+    },
     extend: {},
   },
   plugins: [],
